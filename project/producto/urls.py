93ca169c873f38829e2d9a045ccefb40a1cbb6a0
Facundo <fakugomez14yt@gmail.com>
2024-04-24 21:14:21 -0300

    add html and code in products

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("productocateforia/create/", views.productocategoria_create, name="productocategoria_create"),
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -7,3 +7,7 @@ def home(request):
     consulta_productos = models.ProductoCategoria.objects.all()
     context = {"productos": consulta_productos}
     return render(request, "producto/index.html", context)
+
+
+def productocategoria_create(request):
+    return render(request, "producto/productocategoria_create.html")
